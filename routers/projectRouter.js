3e4806377dfe4f17e77020644a3d69793384c8f8
Edgar F <eflores.ef@gmail.com>
2019-05-18 10:34:39 -0700

    Projects Router CRUD methods.

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -2,6 +2,63 @@ const express = require('express');
 const Projects = require('../data/helpers/projectModel');
 const router = express.Router();
 
+router.get('/', async (req, res) => { // RETURNS NULL
+   try {
+      const projects = await Projects.get(req.query);
+      res.status(200).json(projects)
+   } catch {
+      res.status(500).json({error: "Error retrieving data"})
+   }
+})
 
+router.get('/:id', async (req, res) => {
+   const { id } = req.params;
+   try {
+      const project = await Projects.get(id);
+      res.status(200).json(project)
+   } catch {
+      res.status(500).json({error: "Error retrieving data"})
+   }
+})
+
+router.post('/', async (req, res) => {
+   try {
+      const project = await Projects.insert(req.body);
+      res.status(201).json(project);
+   } catch {
+      res.status(500).json({error: "Error posting data"})
+   }
+})
+
+router.delete('/:id', async (req, res) => {
+   const { id } = req.params;
+   try {
+      const project = await Projects.remove(id);
+      res.status(200).json(project)
+   } catch {
+      res.status(500).json({error: "Error deleting data"})
+   }
+})
+
+router.put('/:id', async (req, res) => {
+   const { id } = req.params;
+   const { body } = req;
+   try {
+      const project = await Projects.update(id, body);
+      res.status(200).json(project);
+   } catch {
+      res.status(500).json({error: "Error updating data"})
+   }
+})
+
+router.get('/project-actions/:id', async (req, res) => {
+   const { id } = req.params;
+   try {
+      const project = await Projects.getProjectActions(id);
+      res.status(200).json(project);
+   } catch {
+      res.status(500).json({error: "Error retrieving data"})
+   }
+})
 
 module.exports = router;
